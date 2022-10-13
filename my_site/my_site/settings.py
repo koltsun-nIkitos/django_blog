92e4022b062879c793879a8a2f939196c2867a43
koltsun-nIkitos <60855708+koltsun-nIkitos@users.noreply.github.com>
2022-10-13 20:37:21 +0300

    Update settings.py

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -130,5 +130,5 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'koltsun-nv@yandex.ru'
-EMAIL_HOST_PASSWORD = 'Nikitos2000sey21let.'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
